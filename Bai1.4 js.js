c5624c69f9a9edba476f04929c50cb0320340da6
vanvu2003 <101229037+vanvu2003@users.noreply.github.com>
2022-03-09 21:19:14 +0700

    Add files via upload

---
+++ Bai1.4 js.js
@@ -0,0 +1,3 @@
+document.getElementById('demo1').innerHTML = "Tài liệu học CSS";
+document.getElementById('demo2').innerHTML = "Tài liệu Học MySQL";
+document.getElementById('demo3').innerHTML = "Tài liệu học PHP";
